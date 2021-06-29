6ca9a8ad6c8a3f0c1efd36ea1c7ebf62ab8e0a00
KevinMG99 <86214768+KevinMG99@users.noreply.github.com>
2021-06-28 23:21:40 -0500

    Add files via upload

---
+++ src/components/Cards/ListCards.js
@@ -0,0 +1,26 @@
+import Cards from "./Cards"
+import celebridades from '../../helpers//usuario'
+
+const ListCards = () => {
+
+
+console.log(celebridades[0].name.last)
+
+    return (
+        <div>        
+            <div className="container">
+                <div className="row">
+                {celebridades.map(user =>(    
+                    <div className="col-sm-6 col-md-4 col-lg-3" key={user.login.uuid}>
+                        <Cards user={user}/>
+                    </div>
+                ))}
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListCards
+
